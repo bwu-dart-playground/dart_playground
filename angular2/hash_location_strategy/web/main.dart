41d58cea8d0a6dfbc38b9573e1ffdc2120ab5b90
zoechi <gzoechi@gmail.com>
2015-12-30 18:11:17 +0100

    lots of changes

---
+++ angular2/hash_location_strategy/web/main.dart
@@ -0,0 +1,8 @@
+//import 'package:angular2/core.dart';
+import 'package:angular2/bootstrap.dart';
+import 'package:_template/app_element.dart';
+
+void main() {
+//  enableProdMode();
+  bootstrap(AppElement);
+}
